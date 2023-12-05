9d39d80fbcdb40032b2908e0fd2e827d481adec8
Mykhailo <sharkon90@gmail.com>
2023-12-05 15:43:47 -0500

    adding function for printing card after chose a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ let windEL = $("#wind");
 let humidityEL = $("#humidity");
 let input = $("#search");
 let img = $("#img")
+let cardGroupEL = $("#card-group")
 
 sectionIDEL.on("click", ".btn", addButton);
 
@@ -41,15 +42,52 @@ function creatObject(value){
             return response.json();
         })
         .then(function(data){
-            console.log(data);
-            console.log(data.list[0].main.temp);
-            console.log(data.list[0].main.humidity);
-            console.log(data.list[0].wind.speed);
+            // console.log(data);
+            // console.log(data.list[0].main.temp);
+            // console.log(data.list[0].main.humidity);
+            // console.log(data.list[0].wind.speed);
             cityNameEl.text(`${data.city.name} (${data.list[0].dt_txt})`);
             tempEL.text(`Temp: ${data.list[0].main.temp} °F`);
             windEL.text(`Wind: ${data.list[0].wind.speed} MPH`);
             humidityEL.text(`Humidity: ${data.list[0].main.humidity} %`);
-            img.attr("src", "https://openweathermap.org/img/wn/10d@2x.png")
+            img.attr("src", `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`)
+            cardPrinter(data)
         })
         })
+    }
+
+    // function to add card for future day
+
+    function cardPrinter(value){
+        console.log(value);
+        console.log(value.list[0].main.temp);
+        console.log(value.list[0].main.humidity);
+        console.log(value.list[0].wind.speed);
+        for(let i = 4; i < value.list.length; i = i + 8){
+        let cardEL = $("<section>");
+        cardEL.attr("class", "card");
+        let cardBodyEL = $("<article>");
+        cardBodyEL.attr("class", "card-body");
+        let dateHeadEL = $("<h5>");
+        dateHeadEL.attr("class", "card-title");
+        dateHeadEL.text(`${value.list[i].dt_txt}`);
+        let cardImgEL = $("<img>");
+        cardImgEL.attr("src", `https://openweathermap.org/img/wn/${value.list[i].weather[0].icon}@2x.png`);
+        let cardTempEL = $("<p>");
+        cardTempEL.attr("class", "card-text");
+        cardTempEL.text(`Temp: ${value.list[i].main.temp} °F`);
+        let cardWindEL = $("<p>");
+        cardWindEL.attr("class", "card-text");
+        cardWindEL.text(`Wind: ${value.list[i].wind.speed} MPH`);
+        let cardHumidity = $("<p>");
+        cardHumidity.attr("class", "card-text");
+        cardHumidity.text(`Humidity: ${value.list[i].main.humidity} %`);
+        cardGroupEL.append(cardEL);
+        cardEL.append(cardBodyEL);
+        cardBodyEL.append(dateHeadEL);
+        cardBodyEL.append(cardImgEL);
+        cardBodyEL.append(cardTempEL);
+        cardBodyEL.append(cardWindEL);
+        cardBodyEL.append(cardHumidity);
+        }
     }
